deab0adf48039f7b913bad35a3abcf766c98b067
sardor <alimukhammad1@gmail.com>
2024-11-03 22:54:54 -0500

    2

--- my-ibm-app/src/App.js
+++ my-ibm-app/src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 
+const currDate = new Date();
+
+
 function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <h3>Current Date{currDate.toLocaleDateString()} and time is {currDate.toLocaleTimeString}</h3>
     </div>
   );
 }
